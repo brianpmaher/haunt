9fec91ad6b83347355c7f737b94a75a1308e63d5
Brian Maher <bmaher91@gmail.com>
2025-02-02 01:47:37 -0700

    Use tabs

--- scripts/linux/build-all.sh
+++ scripts/linux/build-all.sh
@@ -7,13 +7,13 @@ echo "Building all..."
 # Build the engine
 ./scripts/linux/build-engine.sh
 if [ $? -ne 0 ]; then
-    exit 1
+	exit 1
 fi
 
 # Build the editor
 ./scripts/linux/build-editor.sh
 if [ $? -ne 0 ]; then
-    exit 1
+	exit 1
 fi
 
 echo "Done building all" 
